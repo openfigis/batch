30817e586d138f805b9d8cfe2839fd3d25786453
erikvaningen <erikvaningen@gmail.com>
2013-10-04 13:44:15 +0200

    Batching

---
+++ figis-flod-batch-integrationtest/src/test/java/org/fao/fi/figis/batch/BatchUpgradeSpeciesUrlIntegrationTest.java
@@ -0,0 +1,41 @@
+package org.fao.fi.figis.batch;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.fao.fi.figis.dao.FigisDao;
+import org.fao.fi.figis.dao.config.FigisDataBaseProducer;
+import org.fao.fi.figis.domain.ref.FicItem;
+import org.flod.client.FlodClientGet;
+import org.jglue.cdiunit.ActivatedAlternatives;
+import org.jglue.cdiunit.AdditionalClasses;
+import org.jglue.cdiunit.CdiRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(CdiRunner.class)
+@AdditionalClasses(FlodClientGet.class)
+@ActivatedAlternatives({ FigisDataBaseProducer.class })
+public class BatchUpgradeSpeciesUrlIntegrationTest {
+
+	@Inject
+	BatchUpgradeSpeciesUrl batch;
+
+	@Inject
+	FigisDao figisDao;
+
+	@Test
+	public void testFillSemanticUrlInSpecies() {
+		batch.fillSemanticUrlInSpecies();
+
+		List<?> list = figisDao.loadObjects(FicItem.class);
+		for (Object object : list) {
+			FicItem ficItem = (FicItem) object;
+			assertNotNull(ficItem.getSemanticId());
+		}
+
+	}
+}
